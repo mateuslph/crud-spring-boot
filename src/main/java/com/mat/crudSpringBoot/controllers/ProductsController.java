0a39be501a4b83a9475b39cf5af3ec6f7e778cea
mateuslph <mlptche@gmail.com>
2023-11-11 20:56:36 -0300

    Projeto inicial com metodo get product ok

---
+++ src/main/java/com/mat/crudSpringBoot/controllers/ProductsController.java
@@ -0,0 +1,15 @@
+package com.mat.crudSpringBoot.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+public class ProductsController {
+    @GetMapping
+    public ResponseEntity getAllProducts() {
+        return ResponseEntity.ok("Deu ok");
+    }
+}
